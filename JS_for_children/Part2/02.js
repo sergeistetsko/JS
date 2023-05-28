ef16a39034399e7d7245f5f14e3cc72e4e287104
sergeistetsko <sergeistetsko2021@yandex.by>
2023-05-28 14:23:09 +0300

    02

---
+++ JS_for_children/Part2/02.js
@@ -0,0 +1,6 @@
+var sillyString = "эЙ, кАК деЛа?";
+var lowerString = sillyString.toLowerCase();
+var firstCharacter = lowerString[0];
+var firstCharacterUpper = firstCharacter.toUpperCase();
+var restOfString = lowerString.slice(1);
+firstCharacterUpper + restOfString;
