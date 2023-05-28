d6471de2db341d1dd3b005618788ba01e27d33f5
sergeistetsko <sergeistetsko2021@yandex.by>
2023-05-28 16:54:24 +0300

    06

---
+++ JS_for_children/Part2/06.js
@@ -0,0 +1,7 @@
+var mySecretNumber = 5;
+var chicoGuess = 3;
+mySecretNumber === chicoGuess;
+var harpoGuess = 7;
+mySecretNumber === harpoGuess;
+var grouchoGuess = 5;
+mySecretNumber === grouchoGuess;
