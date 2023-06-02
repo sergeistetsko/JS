f6961ed9197206abd96c0076d4bd2be7a1e2d23d
sergeistetsko <sergeistetsko2021@yandex.by>
2023-06-02 08:26:32 +0300

    10_1

---
+++ JS_for_children/Part3/10.js
@@ -0,0 +1,12 @@
+var dinosaurs = [
+    "Тираннозавр 1",
+    "Тираннозавр 2",
+    "Тираннозавр 3",
+    "Тираннозавр 4",
+    "Тираннозавр 5",
+    "Тираннозавр 6",
+    "Тираннозавр 7",
+    "Тираннозавр 8",    
+];
+console.log(dinosaurs.length);
+console.log(dinosaurs[dinosaurs.length - 1]);
