f82c9dbded066983f9d0ee221e83ce69f55170da
sergeistetsko <sergeistetsko2021@yandex.by>
2023-06-03 11:21:03 +0300

    25_1

--- JS_for_children/Part6/25.js
+++ JS_for_children/Part6/25.js
@@ -3,5 +3,7 @@ var name = "Николай";
 console.log("Привет, " + name);
 if (name.length > 6) {
     console.log("Ну и длинющее у вас имя!");
+} else {
+    console.log("Имя у вас не из длинных.");
 }
 
