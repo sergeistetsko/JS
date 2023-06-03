ceef5ee2e93c2722deaaa02d47dbf96cbaa1e702
sergeistetsko <sergeistetsko2021@yandex.by>
2023-06-03 11:59:40 +0300

    28

---
+++ JS_for_children/Part6/28.js
@@ -0,0 +1,7 @@
+// Циклы while
+var sheepCountend = 0;
+while (sheepCountend < 10) {
+    console.log("Посчитано овец: " + sheepCountend + "!");
+    sheepCountend++;
+}
+console.log("Хрррррррр-пссс");
