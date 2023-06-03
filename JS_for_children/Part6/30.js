d38cfa16127e25fa037bfc00d640fe0512b830ef
sergeistetsko <sergeistetsko2021@yandex.by>
2023-06-03 12:17:16 +0300

    30

---
+++ JS_for_children/Part6/30.js
@@ -0,0 +1,5 @@
+// Цикл for, массивы и строки
+var animals = ["лев","фламинго","белый медведь","удав"];
+for (var i = 0; i < animals.length; i++) {
+    console.log("В этом зоопарке есть " + animals[i] + ".");
+}
