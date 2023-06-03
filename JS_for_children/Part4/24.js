01836c0cfae3be448df77852eae5716f16486453
sergeistetsko <sergeistetsko2021@yandex.by>
2023-06-03 10:49:28 +0300

    24

---
+++ JS_for_children/Part4/24.js
@@ -0,0 +1,8 @@
+var myCrazyObject = {
+    "name": "Нелепый объект",
+    "some array": [7,9, { purpose: "путаница", number: 123 }, 3.3],
+    "random animal": "Банановая акула"
+};
+myCrazyObject;
+var myAnswer = myCrazyObject["some array"][2]["number"];
+myAnswer;
