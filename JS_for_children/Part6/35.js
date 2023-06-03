62757652085b7a8fe07dff2f27c557525985e487
sergeistetsko <sergeistetsko2021@yandex.by>
2023-06-03 14:13:13 +0300

    35

---
+++ JS_for_children/Part6/35.js
@@ -0,0 +1,6 @@
+// Прекрасные животные
+var animals = ["Кот", "Рыба", "Лемур", "Комодский варан"];
+for (var i = 0; i < (animals.length); i++) {
+    animals[i] = animals[i] + " - прекрасное животное";
+}
+console.log(animals);
