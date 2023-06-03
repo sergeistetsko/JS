1d9cb59ff5b6af3ccbd64d197d385de795a835a9
sergeistetsko <sergeistetsko2021@yandex.by>
2023-06-03 18:26:08 +0300

    39

---
+++ JS_for_children/Part7/39.js
@@ -0,0 +1,6 @@
+var likesCats = confirm("Тебе нравятся кошки?");
+if (likesCats) {
+    console.log("Ты классная кошка!");
+} else {
+    console.log("Что ж, не проблема. Всё равно ты молодец!");
+}
