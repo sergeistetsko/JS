32db5d3d5e8e11834a47b29984d37c2efcd235ca
sergeistetsko <sergeistetsko2021@yandex.by>
2023-06-03 11:33:08 +0300

    26

---
+++ JS_for_children/Part6/26.js
@@ -0,0 +1,14 @@
+// Цепочка из конструкций if... else
+var lemonChicken = false;
+var beefWithBlackBean = false;
+var sweetAndSourPork = false;
+
+if (lemonChicken) {
+    console.log("Отлично! Я буду курицу с лимоном!");
+} else if (beefWithBlackBean) {
+    console.log("Заказываю говядину.");
+} else if (sweetAndSourPork) {
+    console.log("Ладно, закажу свинину.");
+} else {
+    console.log("Что ж, остаётся рис с яйцом.");
+}
