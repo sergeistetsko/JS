1febb28e4fd94a38ef94275423db7c20e2db0e7a
sergeistetsko <sergeistetsko2021@yandex.by>
2023-06-04 09:11:27 +0300

    50

---
+++ JS_for_children/Part8/50.js
@@ -0,0 +1,10 @@
+var medalForScore = function (score) {
+    if (score < 3) {
+        return "Бронзовая";
+    }
+    if (score < 7) {
+        return "Серебряная";       
+    }
+    return "Золотая";
+};
+medalForScore(10);
