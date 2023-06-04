3680020aa4a049d9c29751ec6399dbd03c3c8b74
sergeistetsko <sergeistetsko2021@yandex.by>
2023-06-04 08:22:13 +0300

    45

---
+++ JS_for_children/Part8/45.js
@@ -0,0 +1,6 @@
+var printMultipleTimes = function (howManyTimes, whatToDraw) {
+    for (var i = 0; i < howManyTimes; i++) {
+        console.log(i + " " + whatToDraw);
+    }
+};
+printMultipleTimes (5, "=^.^=");
