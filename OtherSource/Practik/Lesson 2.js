031bdc81eb772d0b72ae37fb8efe59aaff9c10ca
sergeistetsko <sergeistetsko2021@yandex.by>
2023-06-05 08:53:52 +0300

    Lesson 2_6

--- OtherSource/Practik/Lesson 2.js
+++ OtherSource/Practik/Lesson 2.js
@@ -6,6 +6,14 @@ str[str.length-1];
 str.slice(1,3);
 str.toLowerCase();
 str.toUpperCase();
+
 let text = "  СтРоКа  ";
 let str1 = text.toLowerCase().trim();
 str1[0].toUpperCase()+str1.slice(1);
+
+true && true;
+true && false;
+
+true || true;
+true || false;
+
