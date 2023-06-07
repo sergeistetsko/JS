baf756bbda4791dbd4232a8326e2587cd34caf04
sergeistetsko <sergeistetsko2021@yandex.by>
2023-06-07 10:06:37 +0300

    Знакомство с JS

--- JSforСhildren/I/1/01.js
+++
@@ -1,7 +0,0 @@
-// Рисуем столько котиков, сколько захотим!
-var drawCats = function (howManyTimes) {
-    for (var i = 0; i < howManyTimes; i++) {
-        console.log(i + " =^.^= ");
-    }
-};
-/* Рисуем столько котиков, сколько захотим! */
