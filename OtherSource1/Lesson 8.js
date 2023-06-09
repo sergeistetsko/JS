2a6c910456a67a8235f0ce9340d142431bd5e254
sergeistetsko <sergeistetsko2021@yandex.by>
2023-06-09 07:54:09 +0300

    Пример функции

--- OtherSource1/Lesson 8.js
+++ OtherSource1/Lesson 8.js
@@ -4,3 +4,8 @@ function printText() {
     console.log("Hello")
 };
 printText(); // вызов функции
+
+function printText2(name, age) {
+    console.log("Hello, my name is "+ name +", Age: " + age)
+};
+printText2("Сергей",29); // вызов функции
