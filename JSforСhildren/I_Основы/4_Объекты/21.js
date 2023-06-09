2a4a25cf1ecd1e49b1fd63d3ee96743dc861c4b9
sergeistetsko <sergeistetsko2021@yandex.by>
2023-06-09 13:02:07 +0300

    Создание объекта и добавление его в массив

--- JSforСhildren/I_Основы/4_Объекты/21.js
+++
@@ -1,7 +0,0 @@
-var anna = {name: "Анна", age: 11, luckyNumbers: [2,4,8,16]};
-var dave = {name: "Дэйв", age: 5, luckyNumbers: [3,9,40]};
-var kate = {name: "Кейт", age: 9, luckyNumbers: [1,2,3]};
-var friends = [anna,dave,kate];
-friends[1];
-friends[2].name;
-friends[0].luckyNumbers[1];
---
+++ JSforСhildren/I_Основы/4_Объекты/4_4_Создание объектов и добавление их в массив.js
@@ -0,0 +1,15 @@
+// Создание переменных, содержащих объекты
+
+let anna = {name: "Анна", age: 11, luckyNumbers: [2,4,8,16]};
+let dave = {name: "Дэйв", age: 5, luckyNumbers: [3,9,40]};
+let kate = {name: "Кейт", age: 9, luckyNumbers: [1,2,3]};
+
+// Создание переменной, содержащей массив из объектов
+
+let friends = [anna,dave,kate];
+
+// Доступ к каждому элементу массива\объекта
+
+friends[1];
+friends[2].name;
+friends[0].luckyNumbers[1];
