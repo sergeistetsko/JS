569b9bccb77d462495903c66a70accf9af879fdb
sergeistetsko <sergeistetsko2021@yandex.by>
2023-06-09 08:24:08 +0300

    Создадим функцию Фибоначи

--- OtherSource1/Lesson 8.js
+++ OtherSource1/Lesson 8.js
@@ -44,3 +44,15 @@ arr.sort(function(a,b) {
 })
 console.log(a-b);
 
+// Создадим функцию Фибоначчи
+
+function fibonachi(count) {
+    let fib = [];
+    for(let i = 0; i<count; i++) {
+        if (i==0) fib[i] = 1;
+        else if (i==1) fib[i] = 2;
+        else fib[i] = fib[i-2] + fib[i-1];
+    }
+    return fib;
+}
+let f = fibonachi(10);
