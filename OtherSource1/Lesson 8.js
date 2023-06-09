66a9323e1460c4b5858808f3888dc603c65c6c76
sergeistetsko <sergeistetsko2021@yandex.by>
2023-06-09 08:01:04 +0300

    return

--- OtherSource1/Lesson 8.js
+++ OtherSource1/Lesson 8.js
@@ -9,3 +9,15 @@ function printText2(name, age) {
     console.log("Hello, my name is "+ name +", Age: " + age)
 };
 printText2("Сергей",29); // вызов функции
+
+// Функция возвращает значение функции через return
+
+function printText3(name, age) {
+    return "Hello, my name is "+ name +", Age: " + age;
+}
+
+let name = "Sergei";
+let age = 29;
+
+let res = alert(printText3(name, age));
+console(res);
