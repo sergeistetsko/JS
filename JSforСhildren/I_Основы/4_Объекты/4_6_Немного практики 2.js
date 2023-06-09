f628545768eaea04fcbbd888a2057f2220f100ea
sergeistetsko <sergeistetsko2021@yandex.by>
2023-06-09 13:05:55 +0300

    Немного практики

--- JSforСhildren/I_Основы/4_Объекты/4_6_Немного практики 2.js
+++ JSforСhildren/I_Основы/4_Объекты/4_6_Немного практики 2.js
@@ -1,8 +1,13 @@
-var myCrazyObject = {
+// Создание объекта
+
+let myCrazyObject = {
     "name": "Нелепый объект",
     "some array": [7,9, { purpose: "путаница", number: 123 }, 3.3],
     "random animal": "Банановая акула"
 };
 myCrazyObject;
-var myAnswer = myCrazyObject["some array"][2]["number"];
+
+// Доступ к элементу в объекте
+
+let myAnswer = myCrazyObject["some array"][2]["number"];
 myAnswer;
