573ada7d5535c9ecac5d483825fa91003d701a0b
sergeistetsko <sergeistetsko2021@yandex.by>
2023-06-09 12:20:18 +0300

    угадываем число от 1 до 100

--- OtherSource1/Lesson 8.js
+++ OtherSource1/Lesson 8.js
@@ -110,3 +110,20 @@ function number(number) {
         "Нечётные числа": nechet
     }
 }
+
+// Функция с помощью которого угадываем число от 1 до 100; 10 попыток;
+
+function randomNumber() {
+    let number = Math.floor(Math.random()*100);
+    for (let count = 1; count < 10; count++) {
+        let result = +prompt(`Попытка №${count}, Введите число:`)
+    if (result == number) {
+        return alert(`Вы угадали число! Попыток: ${count}, Число: ${number}`)
+    } else if (result < number) {
+        alert(`Ваше число ${result} меньше загаданного!`)
+    } else if (result > number) {
+        alert(`Ваше число ${result} больше загаданного!`)
+    }
+}  return alert(`Вы не угадали число! Число: ${number}`)
+}
+randomNumber();
