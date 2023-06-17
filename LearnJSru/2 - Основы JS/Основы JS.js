d13e6176e2b61655e417feb5c8316f3733166e83
sergeistetsko <sergeistetsko2021@yandex.by>
2023-06-17 19:24:22 +0300

    Основы JS

---
+++ LearnJSru/2 - Основы JS/Основы JS.js
@@ -0,0 +1,14 @@
+// Основы JS
+// https://learn.javascript.ru/first-steps
+
+// 1 - Привет, мир! Итого
+// https://learn.javascript.ru/hello-world#:~:text=%D0%94%D0%BB%D1%8F%20%D0%B4%D0%BE%D0%B1%D0%B0%D0%B2%D0%BB%D0%B5%D0%BD%D0%B8%D1%8F%20%D0%BA%D0%BE%D0%B4%D0%B0,js%22%3E%3C/script%3E.
+
+// 2 - Структура кода
+// https://learn.javascript.ru/structure
+
+// 3 - Строгий режим — "use strict"
+// https://learn.javascript.ru/strict-mode
+
+// 4 - Переменные
+// https://learn.javascript.ru/variables
